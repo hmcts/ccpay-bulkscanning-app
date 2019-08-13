26d0acdbe297fe50745959de7fafaee799f03d28
Jalal <jalal.deen@hmcts.net>
2019-08-13 17:56:44 +0100

    fixed vault configs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,11 @@ locals {
   ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   asp_name = "ccpay-${var.env}"}"
   sku_size = "${var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"}"
+
+  previewVaultName = "ccpay-aat"
+  nonPreviewVaultName = "ccpay-${var.env}"
+  vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
+
 }
 
 module "bulk-scanning-payment-api" {
@@ -18,7 +23,9 @@ module "bulk-scanning-payment-api" {
   capacity            = "${var.capacity}"
   common_tags         = "${var.common_tags}"
   appinsights_instrumentation_key = "${data.azurerm_key_vault_secret.appinsights_instrumentation_key.value}"
-  asp_name = "${local.asp_name}"
+  appinsights_instrumentation_key = "${data.azurerm_key_vault_secret.appinsights_instrumentation_key.value}"
+
+asp_name = "${local.asp_name}"
   asp_rg = "${local.asp_name}"
   instance_size = "${local.sku_size}"
 
@@ -26,4 +33,12 @@ module "bulk-scanning-payment-api" {
     LOGBACK_REQUIRE_ALERT_LEVEL = "false"
     LOGBACK_REQUIRE_ERROR_CODE  = "false"
   }
+}
+data "azurerm_key_vault" "payment_key_vault" {
+name = "${local.vaultName}"
+resource_group_name = "${var.core_product}-${local.local_env}"
+}
+data "azurerm_key_vault_secret" "appinsights_instrumentation_key" {
+  name = "AppInsightsInstrumentationKey"
+  vault_uri = "${data.azurerm_key_vault.payment_key_vault.vault_uri}"
 }
