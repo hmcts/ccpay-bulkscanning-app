5197410ed7c75de4524da74bd8dd2b2c1bb6131d
Jalal ul Deen <jalal.din@gmail.com>
2019-08-13 13:24:56 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,18 +3,24 @@ provider "azurerm" {
 }
 
 locals {
-  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  asp_name = "ccpay-${var.env}"}"
+  sku_size = "${var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"}"
 }
 
 module "bulk-scanning-payment-api" {
   source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
-  product             = "${var.product}-${var.component}"
+  product             = "${var.product}-api"
   location            = "${var.location_app}"
   env                 = "${var.env}"
   ilbIp               = "${var.ilbIp}"
   subscription        = "${var.subscription}"
   capacity            = "${var.capacity}"
   common_tags         = "${var.common_tags}"
+  appinsights_instrumentation_key = "${data.azurerm_key_vault_secret.appinsights_instrumentation_key.value}"
+  asp_name = "${local.asp_name}"
+  asp_rg = "${local.asp_name}"
+  instance_size = "${local.sku_size}"
 
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = "false"
