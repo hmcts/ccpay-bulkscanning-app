ac0f2138593403711870a7e76f32e45044588c20
Jalal <jalal.deen@hmcts.net>
2019-08-13 16:10:50 +0100

    initial reduced build structure

---
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/payments/ScannedChequeTester.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.bulkscanning.payments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScannedChequeTester {
+
+
+    @Test
+    public void welcomeRootEndpoint() {
+
+        assertThat("Welcome to world of scanning").startsWith("Welcome");
+    }
+}
