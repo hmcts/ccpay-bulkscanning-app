82f8a19c7f02d1009ffa8332190a4e58f955e49a
Amit Chidrewar <amit.chidrewar@hmcts.net>
2019-08-19 20:34:09 +0100

    PAY-1973:Reverting test case

--- src/test/java/uk/gov/hmcts/reform/bulkscanning/payments/ScannedChequeTester.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/payments/ScannedChequeTester.java
@@ -5,18 +5,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ActiveProfiles({"local"})
+//@ActiveProfiles({"local"})
 @SpringBootTest
-//@TestPropertySource(locations="classpath:application-test.properties")
-@TestPropertySource(locations="classpath:application-local.yaml")
+@TestPropertySource(locations="classpath:application-test.properties")
+
 public class ScannedChequeTester {
+
+
     @Test
     public void welcomeRootEndpoint() {
+
         assertThat("Welcome to world of scanning").startsWith("Welcome");
     }
 }
