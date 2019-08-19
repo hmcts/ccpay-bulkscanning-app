d8c7f880d5fddb32141caacc75b6e03bc68a02f1
AyanDuttaHMCTS <Ayan.Dutta@hmcts.net>
2019-08-19 16:36:54 +0100

    PAY-1975: API Implementations for BULK Scan System & Exela

--- src/test/java/uk/gov/hmcts/reform/bulkscanning/payments/ScannedChequeTester.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/payments/ScannedChequeTester.java
@@ -3,11 +3,13 @@ package uk.gov.hmcts.reform.bulkscanning.payments;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles({"local"})
 @SpringBootTest
 public class ScannedChequeTester {
 
