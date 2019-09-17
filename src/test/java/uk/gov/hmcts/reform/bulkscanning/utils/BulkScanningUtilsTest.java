6f07217e81acbd93e105bbf8aaa5642bc3c9af6c
AyanDuttaHMCTS <Ayan.Dutta@hmcts.net>
2019-09-17 18:21:36 +0100

    PAY-2111: Security classes added

--- src/test/java/uk/gov/hmcts/reform/bulkscanning/utils/BulkScanningUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/utils/BulkScanningUtilsTest.java
@@ -14,11 +14,11 @@ import java.time.LocalDateTime;
 
 import static uk.gov.hmcts.reform.bulkscanning.controller.PaymentControllerTest.mockBulkScanningEnvelope;
 
-/*@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("local")
-@TestPropertySource(locations="classpath:application-local.yaml")*/
+@TestPropertySource(locations="classpath:application-local.yaml")
 public class BulkScanningUtilsTest {
 
     @Test
