0e6685dabf2f254a9de7a442b1a6eb60815a9a7e
AyanDuttaHMCTS <Ayan.Dutta@hmcts.net>
2019-09-18 16:58:04 +0100

    PAY-2111: Security classes added

--- src/test/java/uk/gov/hmcts/reform/bulkscanning/validator/BulkScanValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/validator/BulkScanValidatorTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.bulkscanning.validator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.bulkscanning.config.IdamService;
+import uk.gov.hmcts.reform.bulkscanning.config.S2sTokenService;
+import uk.gov.hmcts.reform.bulkscanning.config.TestConfigProperties;
 import uk.gov.hmcts.reform.bulkscanning.model.request.BulkScanPaymentRequest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,8 +37,25 @@ public class BulkScanValidatorTest {
     public static final String CCD_REFERENCE_MISSING = "CCD reference is missing";
     public static final String PAYMENT_DCN_MISSING = "Payment DCN are missing";
 
-    String s2sToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjbWMiLCJleHAiOjE1MzMyMzc3NjN9.3iwg2cCa1_G9-TAMupqsQsIVBMWg9ORGir5xZyPhDabk09Ldk0-oQgDQq735TjDQzPI8AxL1PgjtOPDKeKyxfg[akiss@reformMgmtDevBastion02";
+    @Autowired
+    private TestConfigProperties testProps;
 
+    @Autowired
+    private IdamService idamService;
+
+    @Autowired
+    private S2sTokenService s2sTokenService;
+
+    private static String SERVICE_TOKEN;
+    private static boolean TOKENS_INITIALIZED;
+
+    @Before
+    public void setUp() {
+        if (!TOKENS_INITIALIZED) {
+            SERVICE_TOKEN = s2sTokenService.getS2sToken(testProps.s2sServiceName, testProps.s2sServiceSecret);
+            TOKENS_INITIALIZED = true;
+        }
+    }
 
     @Test()
     @Transactional
@@ -44,7 +65,7 @@ public class BulkScanValidatorTest {
             ,null,null, false);
 
         ResultActions resultActions = mockMvc.perform(post("/bulk-scan-payments/")
-            .header("ServiceAuthorization", s2sToken)
+            .header("ServiceAuthorization", SERVICE_TOKEN)
             .content(asJsonString(bulkScanPaymentRequest))
             .contentType(MediaType.APPLICATION_JSON));
 
