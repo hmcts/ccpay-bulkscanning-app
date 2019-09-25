a5a34f4a93507dfe73c510ce8553c62b157dfd79
AyanDuttaHMCTS <Ayan.Dutta@hmcts.net>
2019-09-25 10:49:40 +0100

    PAY-2111: S2S Token Integration

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -9,9 +9,3 @@ output "idam_api_url" {
 output "s2s_url" {
   value = "${local.s2sUrl}"
 }
-
-# this variable will be accessible to tests as API_GATEWAY_URL environment variable
-output "api_gateway_url" {
-  value = "https://core-api-mgmt-${var.env}.azure-api.net/${local.api_base_path}"
-}
-
