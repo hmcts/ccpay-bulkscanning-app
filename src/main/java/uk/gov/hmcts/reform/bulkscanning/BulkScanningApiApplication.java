c5558021a4d7b82f6d4c1d4f71b5cbfd8c4757f4
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2019-11-22 15:27:10 +0000

    PAY:1995 : Database locking configuration

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/BulkScanningApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/BulkScanningApiApplication.java
@@ -1,13 +1,27 @@
 package uk.gov.hmcts.reform.bulkscanning;
 
+import liquibase.configuration.GlobalConfiguration;
+import liquibase.configuration.LiquibaseConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class BulkScanningApiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BulkScanningApiApplication.class);
 
     public static void main(final String[] args) {
-        SpringApplication.run(BulkScanningApiApplication.class, args);
+        try{
+            //Setting Liquibase DB Lock property before Spring starts up.
+            LiquibaseConfiguration.getInstance()
+                .getConfiguration(GlobalConfiguration.class)
+                .setUseDbLock(true);
+            SpringApplication.run(BulkScanningApiApplication.class, args);
+        }catch (RuntimeException re) {
+            LOG.error("Application crashed with error message: ", re);
+        }
+
     }
 }
