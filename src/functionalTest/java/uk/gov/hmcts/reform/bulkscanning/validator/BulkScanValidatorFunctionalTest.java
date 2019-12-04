fb52d4e353c82f49946b9eb029ba417c5fcd1ed2
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2019-12-04 16:06:57 +0000

    PAY:1995 : Functional Test data refactored.

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanning/validator/BulkScanValidatorFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanning/validator/BulkScanValidatorFunctionalTest.java
@@ -27,9 +27,9 @@ import static uk.gov.hmcts.reform.bulkscanning.controller.PaymentControllerFunct
 @TestPropertySource(locations = "classpath:application-test.yaml")
 public class BulkScanValidatorFunctionalTest {
 
-    public static final String RESPONSIBLE_SERVICE_ID_MISSING = "Responsible service id is missing";
-    public static final String CCD_REFERENCE_MISSING = "CCD reference is missing";
-    public static final String PAYMENT_DCN_MISSING = "Payment DCN are missing";
+    public static final String RESPONSIBLE_SERVICE_ID_MISSING = "site_id can't be Blank";
+    public static final String CCD_REFERENCE_MISSING = "ccd_case_number can't be Blank";
+    public static final String PAYMENT_DCN_MISSING = "document_control_numbers can't be Blank";
 
     @Autowired
     private TestConfigProperties testProps;
