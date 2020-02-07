c482a809d42df21ba215e91986d436e67c8b49b5
Ayan.Dutta <Ayan.Dutta@hmcts.net>
2020-02-07 20:20:37 +0000

    PAY-2639 : Sonar Configuration

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/model/dto/BaseReportData.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/model/dto/BaseReportData.java
@@ -1,16 +1,7 @@
 package uk.gov.hmcts.reform.bulkscanning.model.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-@Builder(builderMethodName = "buildWith")
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class BaseReportData {
     private String respServiceId;
--- src/test/java/uk/gov/hmcts/reform/bulkscanning/utils/BulkScanningUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/utils/BulkScanningUtilsTest.java
@@ -3,15 +3,23 @@ package uk.gov.hmcts.reform.bulkscanning.utils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.bulkscanning.model.entity.StatusHistory;
+import uk.gov.hmcts.reform.bulkscanning.model.entity.Envelope;
+import uk.gov.hmcts.reform.bulkscanning.model.entity.EnvelopeCase;
+import uk.gov.hmcts.reform.bulkscanning.model.entity.EnvelopePayment;
+import uk.gov.hmcts.reform.bulkscanning.model.enums.PaymentStatus;
+import uk.gov.hmcts.reform.bulkscanning.model.repository.EnvelopeRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static uk.gov.hmcts.reform.bulkscanning.controller.PaymentControllerTest.mockBulkScanningEnvelope;
+import static uk.gov.hmcts.reform.bulkscanning.service.PaymentServiceTest.CCD_CASE_REFERENCE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,16 +28,35 @@ import static uk.gov.hmcts.reform.bulkscanning.controller.PaymentControllerTest.
 //@TestPropertySource(locations="classpath:application-local.yaml")
 public class BulkScanningUtilsTest {
 
+    @Autowired
+    private BulkScanningUtils bulkScanningUtils;
+
+    @Autowired
+    private EnvelopeRepository envelopeRepository;
+
     @Test
     public void testInsertStatusHistoryTest() {
-        StatusHistory statusHistory = StatusHistory
-            .statusHistoryWith().
-                envelope(mockBulkScanningEnvelope()).
-                 id(1).
-                dateUpdated(LocalDateTime.now()).
-                dateCreated(LocalDateTime.now()).
-                build();
+        Envelope bsEnvelope =  Envelope.envelopeWith().dateCreated(LocalDateTime.now()).build();
 
-        Assert.assertNotNull(statusHistory);
+        EnvelopePayment payment1 = EnvelopePayment.paymentWith()
+            .dcnReference("888888888888888888888")
+            .envelope(bsEnvelope)
+            .paymentStatus(PaymentStatus.INCOMPLETE.toString())
+            .dateCreated(LocalDateTime.now()).build();
+
+        List<EnvelopePayment> envelopePaymentList = new ArrayList<>();
+        envelopePaymentList.add(payment1);
+
+        EnvelopeCase envelopeCase = EnvelopeCase.caseWith().id(1).ccdReference(CCD_CASE_REFERENCE).envelope(bsEnvelope).dateCreated(LocalDateTime.now()).build();
+
+        List<EnvelopeCase> envelopeCasesList = new ArrayList<>();
+        envelopeCasesList.add(envelopeCase);
+
+        bsEnvelope.setEnvelopePayments(envelopePaymentList);
+        bsEnvelope.setEnvelopeCases(envelopeCasesList);
+        bsEnvelope.setDateCreated(LocalDateTime.now());
+
+        Envelope envelope = bulkScanningUtils.insertStatusHistoryAudit(bsEnvelope);
+        Assert.assertEquals(1, envelope.getEnvelopePayments().size());
     }
 }
