83488b6eac8d32dd6d2c71e03c4025cd7229679c
463952 <subroto.mukherjee@HMCTS.NET>
2020-05-13 11:32:33 +0100

    PAY-2808

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/mapper/PaymentDtoMapper.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/mapper/PaymentDtoMapper.java
@@ -7,7 +7,7 @@ import uk.gov.hmcts.reform.bulkscanning.model.request.BulkScanPayment;
 
 import java.util.Optional;
 
-import static uk.gov.hmcts.reform.bulkscanning.model.enums.EnvelopeSource.Exela;
+import static uk.gov.hmcts.reform.bulkscanning.model.enums.EnvelopeSource.Fee_and_Pay;
 
 @Component
 public class PaymentDtoMapper {
@@ -16,7 +16,7 @@ public class PaymentDtoMapper {
         if(Optional.ofNullable(bulkScanPayment).isPresent()) {
             return PaymentDto.paymentDtoWith()
                 .dcnReference(dcnReference)
-                .source(Exela.toString())
+                .source(Fee_and_Pay.toString())
                 .paymentStatus(PaymentStatus.INCOMPLETE)
                 .build();
         }else {
