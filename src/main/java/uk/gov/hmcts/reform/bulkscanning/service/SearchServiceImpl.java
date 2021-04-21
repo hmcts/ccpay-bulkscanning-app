22b69fb72c3ca8f3a23ba38578ed84a7aa25da79
arbab-cog <arbab.ahmed@hmcts.net>
2021-04-21 12:08:24 +0100

    Fixed searchController incorrect error code

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/service/SearchServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/service/SearchServiceImpl.java
@@ -162,6 +162,6 @@ public class SearchServiceImpl implements SearchService {
             }
             return Collections.emptyList();
         }
-        return Collections.emptyList();
+        return null;
     }
 }
