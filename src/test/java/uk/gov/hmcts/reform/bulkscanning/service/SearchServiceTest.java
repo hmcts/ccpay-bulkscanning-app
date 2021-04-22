9ea66f59cce13f3699b29b5357327edfe0ea3181
arbab-cog <arbab.ahmed@hmcts.net>
2021-04-22 14:03:28 +0100

    fix

--- src/test/java/uk/gov/hmcts/reform/bulkscanning/service/SearchServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanning/service/SearchServiceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.bulkscanning.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -49,8 +48,6 @@ public class SearchServiceTest {
 
     private SearchService paymentService;
 
-    @InjectMocks
-    private SearchServiceImpl paymentServiceImpl;
 
     @MockBean
     private PaymentRepository paymentRepository;
