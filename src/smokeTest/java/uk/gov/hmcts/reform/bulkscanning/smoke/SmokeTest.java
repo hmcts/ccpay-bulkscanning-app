a35793f5f8dc5b561e770e7f0b27eb853c08ef2a
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-07-28 09:34:37 +0100

    compress jar updated

--- src/smokeTest/java/uk/gov/hmcts/reform/bulkscanning/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscanning/smoke/SmokeTest.java
@@ -2,9 +2,8 @@ package uk.gov.hmcts.reform.bulkscanning.smoke;
 
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,12 +15,11 @@ import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @RunWith(SpringRunner.class)
 @Slf4j
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SmokeTest {
     @Value("${TEST_URL:http://localhost:8080}")
     private String testUrl;
 
-    @BeforeEach
+    @Before
     public void setUp() {
         RestAssured.baseURI = testUrl;
     }
