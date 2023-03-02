8ce2fa824168f41fbae2f13fb9303df51099554a
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2023-03-02 11:01:33 +0000

    PAY-5698 (#442)
    * Update ResponsibleSiteId.java
    
    * Update BulkScanPaymentRequest.java
    
    * Update ResponsibleSiteId.java
    
    * Testing rebuild
    
    * sync up with master (#451)
    
    * Upgraded Terraform version to 1.3.7 (#449)
    
    Co-authored-by: “sayali-salunkhe” <“sayali.salunkhe@HMCTS.net”>
    
    * cve fix (#450)
    
    * cve fix
    
    * cve fix
    
    ---------
    
    Co-authored-by: Sayali Salunkhe <83658952+sayali-salunkhe@users.noreply.github.com>
    Co-authored-by: “sayali-salunkhe” <“sayali.salunkhe@HMCTS.net”>
    
    * sync up with master (#454)
    
    * Upgraded Terraform version to 1.3.7 (#449)
    
    Co-authored-by: “sayali-salunkhe” <“sayali.salunkhe@HMCTS.net”>
    
    * cve fix (#450)
    
    * cve fix
    
    * cve fix
    
    * fixing cve
    
    ---------
    
    Co-authored-by: Sayali Salunkhe <83658952+sayali-salunkhe@users.noreply.github.com>
    Co-authored-by: “sayali-salunkhe” <“sayali.salunkhe@HMCTS.net”>
    Co-authored-by: “larslunde-hmcts” <“lars.lunde@HMCTS.net”>
    Co-authored-by: Dave Jones <38425925+davejones74@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: “larslunde-hmcts” <“lars.lunde@HMCTS.net”>
    Co-authored-by: Anooj Kurup <98891865+anoojhmcts@users.noreply.github.com>
    Co-authored-by: Sayali Salunkhe <83658952+sayali-salunkhe@users.noreply.github.com>
    Co-authored-by: “sayali-salunkhe” <“sayali.salunkhe@HMCTS.net”>
    Co-authored-by: Dave Jones <38425925+davejones74@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/model/enums/ResponsibleSiteId.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/model/enums/ResponsibleSiteId.java
@@ -3,8 +3,8 @@ package uk.gov.hmcts.reform.bulkscanning.model.enums;
 public enum ResponsibleSiteId {
     AA07("Divorce"),
     AA08("Probate"),
-    AA09("Financial Remedy");
-
+    AA09("Financial Remedy"),
+    ABA1("Divorce");
 
     private String value;
 
