2a47c5abedc4d83433750352aec14574147e0d87
it.consulting@tutanota.com <it.consulting@tutanota.com>
2023-07-19 10:43:50 +0100

    PAY-6021: Pre-Requisite: Upgrade Spring Boot & App Insights    [PAY-6021] (https://tools.hmcts.net/jira/browse/PAY-6021).

--- src/main/java/uk/gov/hmcts/reform/bulkscanning/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanning/config/SwaggerConfiguration.java
@@ -3,18 +3,52 @@ package uk.gov.hmcts.reform.bulkscanning.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springdoc.core.GroupedOpenApi;
+
 
 @Configuration
 public class SwaggerConfiguration {
+
+    private static final String HEADER = "header";
 
     @Bean
     public GroupedOpenApi api() {
 
         return GroupedOpenApi.builder()
-            .group("payment")
+            .group("bulkscan")
             .packagesToScan("uk.gov.hmcts.reform.bulkscanning.controller")
             .pathsToMatch("/**")
+            .addOperationCustomizer(authorizationHeaders())
             .build();
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().components(new Components())
+            .info(new Info().title("Bulkscanning App").version("1.0.0"));
+    }
+
+    @Bean
+    public OperationCustomizer authorizationHeaders() {
+        return (operation, handlerMethod) ->
+            operation
+                .addParametersItem(
+                    mandatoryStringParameter("Authorization", "User authorization header"))
+                .addParametersItem(
+                    mandatoryStringParameter("ServiceAuthorization", "Service authorization header"));
+    }
+
+    private Parameter mandatoryStringParameter(String name, String description) {
+        return new Parameter()
+            .name(name)
+            .description(description)
+            .required(true)
+            .in(HEADER)
+            .schema(new StringSchema());
     }
 }
