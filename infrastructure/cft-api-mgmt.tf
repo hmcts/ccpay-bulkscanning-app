4c58506b0aa672bec357b83d4ef3dbd8420a3fe2
David Jones <dave.jones@hmcts.net>
2024-06-14 16:01:43 +0100

    Correction to local variable name.

--- infrastructure/cft-api-mgmt.tf
+++ infrastructure/cft-api-mgmt.tf
@@ -1,13 +1,13 @@
 locals {
   cft_api_mgmt_name     = join("-", ["cft-api-mgmt", var.env])
-  cft_api_mgmt_rg_cft   = join("-", ["cft", var.env, "network-rg"])
+  cft_api_mgmt_rg       = join("-", ["cft", var.env, "network-rg"])
 }
 
 resource "azurerm_template_deployment" "cft-bulk-scanning-payment" {
   template_body       = data.template_file.api_template.rendered
   name                = "cft-bulk-scanning-payment-${var.env}"
   deployment_mode     = "Incremental"
-  resource_group_name = local.cft_api_mgmt_rg_cft
+  resource_group_name = local.cft_api_mgmt_rg
   count               = var.env != "preview" ? 1: 0
 
   parameters = {
