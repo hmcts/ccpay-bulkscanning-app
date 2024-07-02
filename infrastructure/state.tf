6ac644e3ee7f64d13dff12fa06d69c480b5348fa
David Jones <dave.jones@hmcts.net>
2024-07-02 17:36:12 +0100

    PAY-6602: Update azurerm.

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -2,14 +2,13 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.70"
-    }
-
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 3.110.0"
+      }
+      azuread = {
+        source  = "hashicorp/azuread"
+        version = "2.53.1"
+      }
   }
 }
