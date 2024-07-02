8ae67954c69522441439b739896d222abb9bae31
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-07-02 15:17:56 +0000

    Updating Terraform Formatting

--- infrastructure/cft-api-mgmt.tf
+++ infrastructure/cft-api-mgmt.tf
@@ -5,9 +5,9 @@ provider "azurerm" {
 
 locals {
   cft_api_mgmt_suffix = var.apim_suffix == "" ? var.env : var.apim_suffix
-  cft_api_mgmt_name     = join("-", ["cft-api-mgmt", local.cft_api_mgmt_suffix])
-  cft_api_mgmt_rg       = join("-", ["cft", var.env, "network-rg"])
-  api_base_path = "bulk-scanning-payment"
+  cft_api_mgmt_name   = join("-", ["cft-api-mgmt", local.cft_api_mgmt_suffix])
+  cft_api_mgmt_rg     = join("-", ["cft", var.env, "network-rg"])
+  api_base_path       = "bulk-scanning-payment"
 }
 
 resource "azurerm_template_deployment" "cft-bulk-scanning-payment" {
@@ -15,14 +15,14 @@ resource "azurerm_template_deployment" "cft-bulk-scanning-payment" {
   name                = "cft-bulk-scanning-payment-${var.env}"
   deployment_mode     = "Incremental"
   resource_group_name = local.cft_api_mgmt_rg
-  count               = var.env != "preview" ? 1: 0
+  count               = var.env != "preview" ? 1 : 0
 
   parameters = {
-    apiManagementServiceName  = local.cft_api_mgmt_rg
-    apiName                   = "bulk-scanning-payment-api"
-    apiProductName            = "bulk-scanning-payment"
-    serviceUrl                = "http://ccpay-bulkscanning-api-${var.env}.service.core-compute-${var.env}.internal"
-    apiBasePath               = local.api_base_path
-    policy                    = data.template_file.policy_template.rendered
+    apiManagementServiceName = local.cft_api_mgmt_rg
+    apiName                  = "bulk-scanning-payment-api"
+    apiProductName           = "bulk-scanning-payment"
+    serviceUrl               = "http://ccpay-bulkscanning-api-${var.env}.service.core-compute-${var.env}.internal"
+    apiBasePath              = local.api_base_path
+    policy                   = data.template_file.policy_template.rendered
   }
 }
