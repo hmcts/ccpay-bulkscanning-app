564a0a11958c75f3196a47cab4a8b777b2e538bb
David Jones <dave.jones@hmcts.net>
2024-07-02 16:10:19 +0100

    PAY-6602: Update to add new APIM instance.

---
+++ infrastructure/cft-api-mgmt.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+locals {
+  cft_api_mgmt_suffix = var.apim_suffix == "" ? var.env : var.apim_suffix
+  cft_api_mgmt_name     = join("-", ["cft-api-mgmt", local.cft_api_mgmt_suffix])
+  cft_api_mgmt_rg       = join("-", ["cft", var.env, "network-rg"])
+  api_base_path = "bulk-scanning-payment"
+}
+
+resource "azurerm_template_deployment" "cft-bulk-scanning-payment" {
+  template_body       = data.template_file.api_template.rendered
+  name                = "cft-bulk-scanning-payment-${var.env}"
+  deployment_mode     = "Incremental"
+  resource_group_name = local.cft_api_mgmt_rg
+  count               = var.env != "preview" ? 1: 0
+
+  parameters = {
+    apiManagementServiceName  = local.cft_api_mgmt_rg
+    apiName                   = "bulk-scanning-payment-api"
+    apiProductName            = "bulk-scanning-payment"
+    serviceUrl                = "http://ccpay-bulkscanning-api-${var.env}.service.core-compute-${var.env}.internal"
+    apiBasePath               = local.api_base_path
+    policy                    = data.template_file.policy_template.rendered
+  }
+}
