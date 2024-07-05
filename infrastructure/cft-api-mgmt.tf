a12da04d50292b50a6f4484935c91bdf597b482b
David Jones <dave.jones@hmcts.net>
2024-07-05 11:39:19 +0100

    Testing bulkscan APIM changes.

--- infrastructure/cft-api-mgmt.tf
+++ infrastructure/cft-api-mgmt.tf
@@ -4,6 +4,10 @@ locals {
   cft_api_mgmt_rg     = join("-", ["cft", var.env, "network-rg"])
 }
 
+data "azurerm_resource_group" "cft_api_mgmt_infra" {
+  name = local.cft_api_mgmt_rg
+}
+
 module "cft_api_mgmt_product" {
   source        = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
   name          = var.product_name
@@ -18,7 +22,7 @@ resource "azurerm_template_deployment" "cft-bulk-scanning-payment" {
   template_body       = data.template_file.api_template.rendered
   name                = "cft-bulk-scanning-payment-${var.env}"
   deployment_mode     = "Incremental"
-  resource_group_name = local.cft_api_mgmt_rg
+  resource_group_name = data.azurerm_resource_group.cft_api_mgmt_infra.name
   count               = var.env != "preview" ? 1 : 0
 
   parameters = {
