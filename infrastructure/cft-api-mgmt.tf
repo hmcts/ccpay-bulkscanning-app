1a707f624ec9ebb440c5162ca3eed8679abac5f7
David Jones <dave.jones@hmcts.net>
2024-07-12 11:05:56 +0100

    PAY-6602: Update api-policy

--- infrastructure/cft-api-mgmt.tf
+++ infrastructure/cft-api-mgmt.tf
@@ -9,13 +9,13 @@ locals {
 }
 
 data "template_file" "cft_policy_template" {
-  template = file("${path.module}/template/cft-api-policy.xml")
+  template = file("${path.module}/template/api-policy.xml")
 
   vars = {
-    allowed_certificate_thumbprints = "${local.thumbprints_in_quotes_str}"
-    s2s_client_id                   = "${data.azurerm_key_vault_secret.s2s_client_id.value}"
-    s2s_client_secret               = "${data.azurerm_key_vault_secret.s2s_client_secret.value}"
-    s2s_base_url                    = "${local.s2sUrl}"
+    allowed_certificate_thumbprints = local.thumbprints_in_quotes_str
+    s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
+    s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
+    s2s_base_url                    = "local.s2sUrl
   }
 }
 
