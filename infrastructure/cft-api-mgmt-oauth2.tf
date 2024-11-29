21f9a0d1c5bc91521280f26911c7bfe2a064290e
Dave Jones <dave.jones@hmcts.net>
2024-11-29 16:03:03 +0000

    PAY-7482: Update revision.

--- infrastructure/cft-api-mgmt-oauth2.tf
+++ infrastructure/cft-api-mgmt-oauth2.tf
@@ -46,7 +46,7 @@ module "cft_api_mgmt_oauth2_api" {
   protocols             = ["http", "https"]
   content_format        = "openapi-link"
   subscription_required = "false"
-  revision              = "1"
+  revision              = "2"
   providers = {
     azurerm = azurerm.aks-cftapps
   }
