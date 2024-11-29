e04cddc2cfd2152404e112c1af13655cee12309e
Dave Jones <dave.jones@hmcts.net>
2024-11-29 18:15:56 +0000

    PAY-7482: fix policy file.

--- infrastructure/cft-api-mgmt-oauth2.tf
+++ infrastructure/cft-api-mgmt-oauth2.tf
@@ -11,12 +11,12 @@ data "template_file" "cft_oauth2_policy_template" {
   template = file("${path.module}/template/cft-api-policy-oauth2-2.xml")
 
   vars = {
-    tenant_id         = data.azurerm_key_vault_secret.tenant_id.value
-    client_id         = data.azurerm_key_vault_secret.apim_client_id.value
-    app_id            = data.azurerm_key_vault_secret.apim_app_id.value
-    s2s_client_id     = data.azurerm_key_vault_secret.s2s_client_id.value
-    s2s_client_secret = data.azurerm_key_vault_secret.s2s_client_secret.value
-    s2s_base_url      = local.s2sUrl
+    cft_oauth2_tenant_id = data.azurerm_key_vault_secret.tenant_id.value
+    cft_oauth2_client_id = data.azurerm_key_vault_secret.apim_client_id.value
+    cft_oauth2_app_id    = data.azurerm_key_vault_secret.apim_app_id.value
+    s2s_client_id        = data.azurerm_key_vault_secret.s2s_client_id.value
+    s2s_client_secret    = data.azurerm_key_vault_secret.s2s_client_secret.value
+    s2s_base_url         = local.s2sUrl
   }
 }
 
