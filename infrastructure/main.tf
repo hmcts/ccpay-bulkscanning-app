3efaa43a4f9003ae6a3d7db2c6e60f761644c92b
Ollie-Smith <oliver.smith@hmcts.net>
2025-04-22 13:57:38 +0100

    PAY-7766 removal of azure extension

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,7 +48,7 @@ module "ccpay-bulkscanning-payment-database-v15" {
   pgsql_server_configuration = [
     {
       name  = "azure.extensions"
-      value = "plpgsql,pg_stat_statements,pg_buffercache"
+      value = "pg_stat_statements,pg_buffercache"
     }
   ]
   admin_user_object_id = var.jenkins_AAD_objectId
