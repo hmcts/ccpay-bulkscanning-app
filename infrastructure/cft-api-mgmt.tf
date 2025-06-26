6f1602a79a397a41e5aa84af35a6e28cdb4d02fb
Dave Jones <dave.jones@hmcts.net>
2025-06-26 11:06:24 +0100

    Tidy up of policy code.

--- infrastructure/cft-api-mgmt.tf
+++ infrastructure/cft-api-mgmt.tf
@@ -12,9 +12,26 @@ data "template_file" "cft_policy_template" {
   vars = {
     allowed_certificate_thumbprints = local.thumbprints_in_quotes_str
     s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
-    s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
     s2s_base_url                    = local.s2sUrl
   }
+
+  depends_on = [
+    resource.azurerm_api_management_named_value.ccpay_s2s_client_secret
+  ]
+}
+
+resource "azurerm_api_management_named_value" "ccpay_s2s_client_secret" {
+  name                = "ccpay-s2s-client-secret"
+  resource_group_name = local.cft_api_mgmt_rg
+  api_management_name = local.cft_api_mgmt_name
+  display_name        = "ccpay-s2s-client-secret"
+  value               = data.azurerm_key_vault_secret.s2s_client_secret.value
+  secret              = true
+  provider            = azurerm.aks-cftapps
+
+  depends_on = [
+    module.cft_api_mgmt_api
+  ]
 }
 
 module "cft_api_mgmt_product" {
