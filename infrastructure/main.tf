842196da030ed34b5fd53010fa26cb295b443d46
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2026-09-25 16:04:57 +0100

    CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5 (#620)
    * CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5
    
    * CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5
    
    * CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5
    
    * CME-1005: F&P - Automated immutable PostgreSQL DB backups for services with criticality rating 4 & 5
    
    * CME-1005: change terraform-module-postgresql-flexible to master

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,7 +33,7 @@ module "ccpay-bulkscanning-payment-database-v15" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=DTSPO-30107-additional-postgres-admins"
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
   product              = var.product
   component            = var.component
   business_area        = "cft"
@@ -62,6 +62,8 @@ module "ccpay-bulkscanning-payment-database-v15" {
   action_group_name          = join("-", [var.db_monitor_action_group_name, local.db_server_name, var.env])
   email_address_key          = var.db_alert_email_address_key
   email_address_key_vault_id = data.azurerm_key_vault.payment_key_vault.id
+  # Add service criticality param
+  service_criticality        = var.service_criticality
 }
 
 # Populate Vault with DB info
