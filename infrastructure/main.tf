f0b20ea3d698ecd9a2efef5b7b1f2c55ea0c2304
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2026-09-25 09:33:09 +0100

    CME-1005: change terraform-module-postgresql-flexible to master

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,7 +33,7 @@ module "ccpay-bulkscanning-payment-database-v15" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=DTSPO-30107-additional-postgres-admins"
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
   product              = var.product
   component            = var.component
   business_area        = "cft"
